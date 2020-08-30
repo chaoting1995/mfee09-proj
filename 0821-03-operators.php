a624de53a04dc9283a11632b8bde9467d42f6631
chaoting <02115214@gm.scu.edu.tw>
2020-08-31 00:28:09 +0800

    0821-03、04、05、06，複習

--- 0821-03-operators.php
+++ 0821-03-operators.php
@@ -2,10 +2,14 @@
 $a = 13;
 $b = 27;
 
-echo var_dump($a && $b) . '<br>';
-echo var_dump($a and $b) . '<br>';
+// php邏輯運算子，一律拿到布林值
+// 判斷拿到的值是什麼類型：var_dump()
+echo var_dump($a && $b) . '<br>';    //bool(true)
+echo var_dump($a and $b) . '<br>';   //bool(true)
 
-$c = $a && $b;
-echo var_dump($c) . '<br>';
+
+$c = $a && $b; 
+echo var_dump($c) . '<br>';   //bool(true)
 $c = $a and $b;
-echo var_dump($c) . '<br>';
+echo var_dump($c) . '<br>';   //int(13)，不一樣了
+//php優先運算權，指定運算子>文字的邏輯運算子(and)//還是不太懂
--- 0821-04-get_params.php
+++ 0821-04-get_params.php
@@ -1,15 +1,19 @@
 <?php
-// 如何取得用戶端傳來的資料?
-// 內建變數，取得參數a，解析用戶端傳來的資料
+// 如何從php取得用戶端傳來的資料?
+// 取得參數a，解析用戶端傳來的資料
+// 內建變數
+//例如：「http://localhost/mfee09-proj/0821-04-get_params.php?a=100」
 echo $_GET['a'];
+//那a從何來？應該是客戶端表單資料
 
-// 沒資料時，$_GET['a'];會讓網頁跳「Notice: Undefined...」
-// 如何不讓「Notice」出現? 
 
+// 沒資料時，網頁會跳「Notice: Undefined...」，如何不讓「Notice」出現? 
 // 1.傳統寫法：isset()判斷是否有設定；三元運算子設定：如果有就用這個值，沒有設定就填0
-// → isset()、empty()都可以
+// → isset()、empty()都可以 //empty()，有設定，是空值，還是會得到true
+// isset()
 $a = isset($_GET['a']) ? $_GET['a'] : 0;
 echo $a;
+// 有設得值;無設得0
 
 // 2.php7新寫法
 $a = $_GET['a'] ?? 0;
