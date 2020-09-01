3fd239ed0d322cdbcb120de0a67e1951aa3fc58e
chaoting <02115214@gm.scu.edu.tw>
2020-09-01 15:11:57 +0800

    0901進度

--- cate02-navbar.php
+++ cate02-navbar.php
@@ -26,23 +26,25 @@ foreach ($cates as $k => $v) {
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
         <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <?php foreach ($cates as $v) : ?>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="?cate=<?= $v['sid'] ?>" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <?= $v['name'] ?>
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <?php foreach ($v['children'] as $v2) : ?>
-                                <a class="dropdown-item" href="?cate=<?= $v2['sid'] ?>"><?= $v2['name'] ?></a>
-                            <?php endforeach; ?>
-                        </div>
-                    </li>
+                <?php foreach ($cates as $v): ?>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown<?= $v['sid'] ?>" role="button"
+                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <?= $v['name'] ?>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <?php foreach ($v['children'] as $v2): ?>
+                            <a class="dropdown-item" href="?cate=<?= $v2['sid'] ?>"><?= $v2['name'] ?></a>
+                        <?php endforeach; ?>
+                    </div>
+                </li>
                 <?php endforeach; ?>
             </ul>
 
@@ -56,3 +58,4 @@ foreach ($cates as $k => $v) {
 </div>
 <?php include __DIR__ . '/parts/__scripts.php'; ?>
 <?php include __DIR__ . '/parts/__html_foot.php'; ?>
+
