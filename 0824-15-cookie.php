4122110bc3057781b8b58852938767b7ca0a6054
chaoting <02115214@gm.scu.edu.tw>
2020-09-02 22:14:42 +0800

    0824複習close

--- 0824-15-cookie.php
+++ 0824-15-cookie.php
@@ -1,8 +1,12 @@
 <?php
+// 設定cookie，要在所有html出現之前，因為cookie設定在http檔頭，資料都送出才改檔頭，會有問題
+// 緊貼為前面，也不要有換行、空白字元，會被視為html
+// response headers
+// 設定在檔頭，告訴瀏覽器，我要設定cookie
+
 setcookie('mycookie', 2)
 ?>
 
-<!-- 設定cookie，要在html出現之前，因為cookie設定在檔頭，資料都送出    -->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,7 +18,12 @@ setcookie('mycookie', 2)
 
 <body>
     <?php
-
+    // 讀取時可用內建變數，為陣列
+    // request headers
+    // request的時候會把cookie送過去
+    // 讀取的是用戶端request進來的cookie
+    // 還沒傳回來，就讀取，會沒東西
+    // 要回應了，設定才生效
     echo $_COOKIE['mycookie'];
 
     ?>
--- 0824-16-cookie.php
+++ 0824-16-cookie.php
@@ -9,7 +9,8 @@
 </head>
 <body>
 <?php
-
+// 第二支，不設定，直接讀取，也讀的到
+// 之前別支檔案有設定，發request的時候會帶之前的設定過去，cookie可以跨頁面
 echo $_COOKIE['mycookie'];
 
 ?>
